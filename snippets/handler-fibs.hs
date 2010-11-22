12ad614119d45b0a897f00b09d6aaa69939ae8e7
Michael Snoyman <michael@snoyman.com>
2010-11-22 23:50:06 +0200

    Added missing snippets

---
+++ snippets/handler-fibs.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE TypeFamilies, QuasiQuotes, GeneralizedNewtypeDeriving #-}
+import Yesod
+data Fibs = Fibs
+-- START
+newtype Natural = Natural Int -- we might even like to go with Word here
+-- STOP
+    deriving (Show, Read, Eq, Num, Ord)
+-- START
+instance SinglePiece Natural where
+    toSinglePiece (Natural i) = show i
+    fromSinglePiece s =
+        case reads s of
+            (i, _):_
+                | i < 1 -> Left "0 and negative numbers are unnatural" -- hehe
+                | otherwise -> Right $ Natural i
+            [] -> Left "That's not even an integer!"
+-- STOP
+mkYesod "Fibs" [$parseRoutes|
+/fibs/#Natural FibsR GET
+|]
+instance Yesod Fibs where approot _ = ""
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+getFibsR :: Natural -> GHandler Fibs Fibs RepPlain
+getFibsR (Natural i) = return $ RepPlain $ toContent $ show $ fibs !! (i - 1)
+main = basicHandler 3000 Fibs
---
+++ snippets/handler-wiki.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TypeFamilies, QuasiQuotes #-}
+import Yesod
+data Fibs = Fibs
+-- START
+data Page = Page String String [String] -- 2 or more
+instance MultiPiece Page where
+    toMultiPiece (Page x y z) = x : y : z
+    fromMultiPiece (x:y:z) = Right $ Page x y z
+    fromMultiPiece _ = Left "I need at least 2 pieces"
+-- STOP
+main = return ()

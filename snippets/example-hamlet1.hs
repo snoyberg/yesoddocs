4a759476f7bc4c75451b9f0f1d692a8f860b34ae
Greg Weber <greg@gregweber.info>
2011-03-15 15:51:16 -0700

    add starting example for hamlet

---
+++ snippets/example-hamlet1.hs
@@ -0,0 +1,10 @@
+<table #an-id .a-class style=display:none;>
+  <thead>
+    <tr>
+      <th>fruit
+      <th>color
+  <tbody>
+    $forall f <- fruits
+      <tr>
+        <td>#{fruitName f}
+        <td>#{fruitColor f}

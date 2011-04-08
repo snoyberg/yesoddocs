ce484b690a07788f3c95d69840e1106ba4a7b71b
Michael <michael@snoyman.com>
2011-04-08 17:42:12 +0300

    Work on REST chapter

--- snippets/rest-rephtmljson.hs
+++ snippets/rest-rephtmljson.hs
@@ -1,3 +1,4 @@
+-- START
 {-# LANGUAGE QuasiQuotes, TypeFamilies, OverloadedStrings #-}
 {-# LANGUAGE MultiParamTypeClasses, TemplateHaskell #-}
 import Yesod
@@ -6,8 +7,9 @@ mkYesod "R" [$parseRoutes|
 / RootR GET
 /#String NameR GET
 |]
-instance Yesod R where approot _ = ""
--- START
+instance Yesod R where
+    approot _ = ""
+
 getRootR = defaultLayout $ do
     setTitle "Homepage"
     addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"
@@ -22,20 +24,21 @@ $(function(){
 });
 |]
     let names = words "Larry Moe Curly"
-    addHamlet [$hamlet|\
+    addHamlet [$hamlet|
 <div id="results">
-    \Your results will be placed here if you have Javascript enabled.
+    Your results will be placed here if you have Javascript enabled.
 <ul>
     $forall name <- names
         <li>
             <a href="@{NameR name}">#{name}
 |]
+
 getNameR name = do
     let widget = do
             setTitle $ string name
-            addHamlet [$hamlet|\Looks like you have Javascript off. Name: #{name}
-|]
+            addHamlet [$hamlet|Looks like you have Javascript off. Name: #{name}|]
     let json = jsonMap [("name", jsonScalar name)]
     defaultLayoutJson widget json
--- STOP
+
 main = warpDebug 4000 R
+-- STOP
